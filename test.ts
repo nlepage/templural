bbb3158389493cbe8aa38835232f96a81b687e3b
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2021-05-19 15:21:13 +0200

    ✅ Add basic test: One word agrees with one number

--- test.ts
+++ test.ts
@@ -2,6 +2,9 @@ import test from 'ava'
 
 import templural from './src'
 
-test('templural', t => {
-  t.is(templural`test`, 'templural')
+test('One word agrees with one number', t => {
+  t.is(templural`Yoann and Valentin had ${0} interesting idea{s}`, 'Yoann and Valentin had 1 interesting idea')
+  t.is(templural`Yoann and Valentin had ${1} interesting idea{s}`, 'Yoann and Valentin had 1 interesting idea')
+  t.is(templural`Yoann and Valentin had ${2} interesting idea{s}`, 'Yoann and Valentin had 1 interesting ideas')
+  t.is(templural`Yoann and Valentin had ${42} interesting idea{s}`, 'Yoann and Valentin had 1 interesting ideas')
 })
