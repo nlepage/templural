e96a2d9cad45e6437bc2ac74265e611a67db539e
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2021-05-19 16:46:42 +0200

    ✅ The singular of a word is not a prefix of the plural: OK

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 export default function templural(chunks: TemplateStringsArray, ...args: number[]): string {
   return chunks
     .map((chunk, i) => {
-      const replacement = args[i - 1] > 1 ? '$1' : ''
-      return chunk.replace(/\{(.*?)\}/g, replacement)
+      const plural = args[i - 1] > 1
+      return chunk.replace(/\{(.*?)\}/g, (_match, g1) => {
+        const split = g1.split(':')
+        if (split.length === 1) return plural ? g1 : ''
+        return plural ? split[1] : split[0]
+      })
     })
     .reduce((s, chunk, i) => s + args[i - 1] + chunk)
 }
