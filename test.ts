fe6ce5394a0dd6da8a36ca997b77dd59a8ac5805
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2021-05-19 16:22:08 +0200

    ✅ Several words each agree with one word: OK

--- test.ts
+++ test.ts
@@ -19,3 +19,10 @@ test('Several words agree with one number', t => {
   t.is(templural`I just had ${2} bear{s} darling{s}, I swear{s}`, 'I just had 2 bears darlings, I swears')
   t.is(templural`I just had ${6} bear{s} darling{s}, I swear{s}`, 'I just had 6 bears darlings, I swears')
 })
+
+test('Several words each agree with one word', t => {
+  t.is(templural`I bought ${1} carrot{s} and ${1} potato{es}`, 'I bought 1 carrot and 1 potato')
+  t.is(templural`I bought ${1} carrot{s} and ${3} potato{es}`, 'I bought 1 carrot and 3 potatoes')
+  t.is(templural`I bought ${2} carrot{s} and ${1} potato{es}`, 'I bought 2 carrots and 1 potato')
+  t.is(templural`I bought ${2} carrot{s} and ${3} potato{es}`, 'I bought 2 carrots and 3 potatoes')
+})
