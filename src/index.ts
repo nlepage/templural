ad50a3e289f8ae0c6b1aca36ed50a08ed012c3ac
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2021-05-19 15:42:46 +0200

    ✅ One word agrees with one number: OK

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-export default function templural(chunks: TemplateStringsArray, ...args: (number | string)[]): string {
-    return 'templural'
+export default function templural(chunks: TemplateStringsArray, ...args: number[]): string {
+  return chunks
+    .map((chunk, i) => {
+      const replacement = args[i - 1] > 1 ? '$1' : ''
+      return chunk.replace(/\{(.*?)\}/g, replacement)
+    })
+    .reduce((s, chunk, i) => s + args[i - 1] + chunk)
 }
--- test.ts
+++ test.ts
@@ -3,8 +3,8 @@ import test from 'ava'
 import templural from './src'
 
 test('One word agrees with one number', t => {
-  t.is(templural`Yoann and Valentin had ${0} interesting idea{s}`, 'Yoann and Valentin had 1 interesting idea')
+  t.is(templural`Yoann and Valentin had ${0} interesting idea{s}`, 'Yoann and Valentin had 0 interesting idea')
   t.is(templural`Yoann and Valentin had ${1} interesting idea{s}`, 'Yoann and Valentin had 1 interesting idea')
-  t.is(templural`Yoann and Valentin had ${2} interesting idea{s}`, 'Yoann and Valentin had 1 interesting ideas')
-  t.is(templural`Yoann and Valentin had ${42} interesting idea{s}`, 'Yoann and Valentin had 1 interesting ideas')
+  t.is(templural`Yoann and Valentin had ${2} interesting idea{s}`, 'Yoann and Valentin had 2 interesting ideas')
+  t.is(templural`Yoann and Valentin had ${42} interesting idea{s}`, 'Yoann and Valentin had 42 interesting ideas')
 })
