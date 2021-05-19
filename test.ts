dabb5aee05a34e2d8152e774fdb90caae012700b
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2021-05-19 15:52:42 +0200

    ✅ Several words agree with one number: OK

--- test.ts
+++ test.ts
@@ -13,3 +13,9 @@ test('One word agrees with one number', t => {
   t.is(templural`Yoann and Valentin had ${2} interesting idea{s}`, 'Yoann and Valentin had 2 interesting ideas')
   t.is(templural`Yoann and Valentin had ${42} interesting idea{s}`, 'Yoann and Valentin had 42 interesting ideas')
 })
+
+test('Several words agree with one number', t => {
+  t.is(templural`I just had ${1} bear{s} darling{s}, I swear{s}`, 'I just had 1 bear darling, I swear')
+  t.is(templural`I just had ${2} bear{s} darling{s}, I swear{s}`, 'I just had 2 bears darlings, I swears')
+  t.is(templural`I just had ${6} bear{s} darling{s}, I swear{s}`, 'I just had 6 bears darlings, I swears')
+})
