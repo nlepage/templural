c693b2d114430a049d8fa48808d9e913b5733042
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2021-05-19 16:56:19 +0200

    ✅ Several words agree with several numbers: OK

--- test.ts
+++ test.ts
@@ -20,7 +20,7 @@ test('Several words agree with one number', t => {
   t.is(templural`I just had ${6} bear{s} darling{s}, I swear{s}`, 'I just had 6 bears darlings, I swears')
 })
 
-test('Several words each agree with one word', t => {
+test('Several words each agree with one different number', t => {
   t.is(templural`I bought ${1} carrot{s} and ${1} potato{es}`, 'I bought 1 carrot and 1 potato')
   t.is(templural`I bought ${1} carrot{s} and ${3} potato{es}`, 'I bought 1 carrot and 3 potatoes')
   t.is(templural`I bought ${2} carrot{s} and ${1} potato{es}`, 'I bought 2 carrots and 1 potato')
@@ -31,4 +31,11 @@ test('The singular of a word is not a prefix of the plural', t => {
   t.is(templural`${1} {person:people} {is:are} connected`, '1 person is connected')
   t.is(templural`${2} {person:people} {is:are} connected`, '2 people are connected')
   t.is(templural`${666} {person:people} {is:are} connected`, '666 people are connected')
+})
+
+test('Several words agree with several numbers', t => {
+  t.is(templural`${1} dog{s} bark{s:} and ${1} cat{s} meow{s:}`, '1 dog barks and 1 cat meows')
+  t.is(templural`${2} dog{s} bark{s:} and ${1} cat{s} meow{s:}`, '2 dogs bark and 1 cat meows')
+  t.is(templural`${1} dog{s} bark{s:} and ${2} cat{s} meow{s:}`, '1 dog barks and 2 cats meow')
+  t.is(templural`${2} dog{s} bark{s:} and ${2} cat{s} meow{s:}`, '2 dogs bark and 2 cats meow')
 })
