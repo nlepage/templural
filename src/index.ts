5fda5d650ef3f8941173d8406da57a61122764d8
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2021-05-20 00:01:29 +0200

    ✅ The actual number is replaced by words: OK

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
 export default function templural(chunks: TemplateStringsArray, ...args: number[]): string {
+  let inCurlies = false
+
   return chunks.reduce((prev, chunk, i) => {
-    let next = chunk.replace(/\{(.*?)\}/g, (_match, g1) => {
+    let next = chunk
+
+    if (inCurlies) {
+      next = next.replace(/^:(.*?)}/, (_match, g1) => {
+        const plural = args[i - 1] > 1
+        const split = g1.split(':')
+
+        if (split.length === 1) return plural ? g1 : ''
+
+        return plural ? split[1] : split[0]
+      })
+
+      inCurlies = false
+    } else {
+      if (i > 0) next = args[i - 1] + next
+    }
+
+    next = next.replace(/\{(.*?)\}/g, (_match, g1) => {
       let plural = args[i - 1] > 1
       let split = g1.split(':')
 
@@ -15,7 +34,12 @@ export default function templural(chunks: TemplateStringsArray, ...args: number[
       return plural ? split[1] : split[0]
     })
 
-    if (i > 0) next = args[i - 1] + next
+    // FIXME also check it's not the last chunk
+    // using chunk and not next is intentional
+    if (chunk.endsWith('{')) {
+      next = next.slice(0, -1)
+      inCurlies = true
+    }
     
     return prev + next
   }, '')
