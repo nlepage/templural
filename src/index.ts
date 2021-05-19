7556b111ed9352606023f29654e20af294d111a1
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2021-05-19 21:27:15 +0200

    ♻️ Refactor code

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,22 @@
 export default function templural(chunks: TemplateStringsArray, ...args: number[]): string {
-  return chunks
-    .map((chunk, i) => {
-      return chunk.replace(/\{(.*?)\}/g, (_match, g1) => {
-        let plural = args[i - 1] > 1
-        let split = g1.split(':')
+  return chunks.reduce((prev, chunk, i) => {
+    let next = chunk.replace(/\{(.*?)\}/g, (_match, g1) => {
+      let plural = args[i - 1] > 1
+      let split = g1.split(':')
 
-        const indexMatch = /^\$(\d)+$/.exec(split[0])
-        if (indexMatch != null) {
-          plural = args[Number(indexMatch[1]) - 1] > 1
-          split = split.slice(1)
-        }
+      const indexMatch = /^\$(\d)+$/.exec(split[0])
+      if (indexMatch != null) {
+        plural = args[Number(indexMatch[1]) - 1] > 1
+        split = split.slice(1)
+      }
 
-        if (split.length === 1) return plural ? g1 : ''
+      if (split.length === 1) return plural ? g1 : ''
 
-        return plural ? split[1] : split[0]
-      })
+      return plural ? split[1] : split[0]
     })
-    .reduce((s, chunk, i) => s + args[i - 1] + chunk)
+
+    if (i > 0) next = args[i - 1] + next
+    
+    return prev + next
+  }, '')
 }
